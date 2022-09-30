f7cd8401b5a51ee9240006290882fb2b11ea5882
Abdallah <abdallah.moubarak02@gmail.com>
2022-09-30 20:10:19 +0300

    creating api route

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get("/sort", [SortController::class, 'sort']);
+
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
